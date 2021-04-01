8f6ef4044527a68127c60cb490da5c27c5709d3b
zhanna <de-mambo@yandex.ru>
2021-04-01 18:42:53 +0300

    добавила решение для нуля

--- function_2.py
+++ function_2.py
@@ -1,9 +1,9 @@
 def a(n):
-    if n == 1:
+    if n == 1 or n == 0:
         return 1
     else:
         y = n - a(a(n - 1))
         return y
 
-for i in range(1, 10):
+for i in range(0, 10):
     print(a(i), end=' ')
